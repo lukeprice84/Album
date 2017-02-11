8557ae4f7ccec5a75c91cf08aab1e3e823a6e79c
lukeprice84 <lukeprice@mail.com>
2017-02-11 21:06:47 +0000

    1.0

---
+++ album/module/Album/src/Module.php
@@ -0,0 +1,34 @@
+<?php
+namespace Album;
+
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface
+{
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+    
+    public function getServiveConfig()
+    {
+        return [
+            'factories' => [
+                Model\AlbumTable::class => function ($container) {
+                    $tableGateway = $container->get(Model\AlbumTableGateway::class);
+                    return new Model\AlbumTable($tableGateway);
+                },
+                Model\AlbumTableGateway::class => function($container) {
+                    $dbAdapter = $container->get(AdapterInterface::class);
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Model\Album());
+                    return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+                },
+            ],
+        ];
+    }
+}
